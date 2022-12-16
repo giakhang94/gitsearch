1703d250b39a0e0ce4534e86b0135439c595ec88
DESKTOP-3R5KC0E\admin <ngk.khang94@gmail.com>
2022-12-16 21:08:43 +0700

    install needed router dom, tailwind, icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Home from "./pages/home";
 
 function App() {
-  return <div className="App">git search app</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
