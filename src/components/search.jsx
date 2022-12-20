6b50a1f52104ae1413254bcc9e26c7da534a593f
DESKTOP-3R5KC0E\admin <ngk.khang94@gmail.com>
2022-12-20 12:19:37 +0700

    change search input onclick function no blank input

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -10,7 +10,19 @@ export default function Search() {
             <div className=' px-2 py-1 flex items-center laptop:w-[50%] tablet:w-[65%] mobile:w-[80%] smallmobile:w-[80%] min-w-[200px] border border-gray-200 shadow-sm shadow-gray-300 rounded-md'>
                 <span className='text-gray-500 text-lg'><AiOutlineSearch /></span>
                 <input type="text" className=' outline-none text-gray-500 py-1 px-1 pl-5 pr-10 w-full' name="" id=""  value = {input}placeholder='Enter Github User' onChange={(e) => {setInput(e.target.value)}}/>
-            <button className='px-2 py-1 bg-[#7ED4D9] text-white font-semibold text-md rounded-md' onClick={(e) => {e.preventDefault(); gitSearch(input)}}>Search</button>
+            <button 
+                className='px-2 py-1 bg-[#7ED4D9] text-white font-semibold text-md rounded-md' 
+                onClick={(e) => {
+                    e.preventDefault(); 
+                    if(input === "") {
+                        alert('please enter Github user')
+                    }else{
+                        gitSearch(input)
+                    }
+                }}
+                >
+                    Search
+                </button>
             </div>
         </form>
     )
