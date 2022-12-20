ffcbb5ddad27bd20d8a6137fe83aafaca216b79b
DESKTOP-3R5KC0E\admin <ngk.khang94@gmail.com>
2022-12-20 13:45:55 +0700

    change card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,7 +4,7 @@ export default function Card({name, userData, isFollow, small}) {
             <div className={`${!small && "w-20 h-20 min-w-[80px]" } mr-5 ${small && "w-12 h-12 min-w-[48px]"} `}>
                 <img src={userData.avtUrl} alt="" className="rounded-[50%] w-full min-w-10"  />
             </div>
-            <div className="flex-1 flex flex-col min-w-[200px]">
+            <div className="flex-1 flex flex-col min-w-[120px]">
                 <span className={`font-bold ${!small && 'tablet:text-2xl'} ${small && 'tablet:text-xl'} ${!small && 'laptop:text-2xl'} ${small && 'laptop:text-xl'}  text-[#102A42] mobile:text-lg smallmobile:text-lg`}>{name}</span>
                 <span className="text-gray-500 text-sm">
                     {!isFollow && <a href={userData.login}>{userData.login}</a>}
