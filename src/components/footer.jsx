284ce79f28f78210444a8931b72dde916391281e
DESKTOP-3R5KC0E\admin <ngk.khang94@gmail.com>
2022-12-20 11:34:48 +0700

    github users add s for user

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className="footer bg-slate-600 text-gray-300 flex flex-col items-center justify-center py-2 mt-10">
-            <h1 className="font-semibold text-md">Github User Search Application</h1>
+            <h1 className="font-semibold text-md">Github Users Search Application</h1>
             <p className="italic text-sm">Khang Nguyen - ngk.khang94@gmail.com</p>
         </div>
     )
